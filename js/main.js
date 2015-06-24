f268f7b31a2f8c1129e6a37cfae6941ecf6ce060
emmacunningham <the.cunning.ham@gmail.com>
2015-06-23 18:17:08 -0700

    data fetching and handling for both movie list and details

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 var RESULTS_CONTAINER = 'results-container';
+var SEARCH_TERM_CONTAINER = 'search-term';
+var DETAILS_CONTAINER = 'details-container';
 
 var getElementById = function(id) {
   return document.getElementById(id);
 };
-
-var getElementBySelector = function(selector) {
-  return document.querySelector(selector);
-}
 
 var makeAjaxRequest = function(url, params) {
   var request = new XMLHttpRequest();
@@ -43,22 +41,71 @@ var makeSearchRequest = function(searchTerm) {
 
 };
 
+var makeMovieRequest = function(id) {
+
+  var url = 'http://www.omdbapi.com/?i=' + id + '&plot=full&r=json';
+  var onLoad = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      var resp = this.response;
+      handleMovieResults(JSON.parse(resp));
+
+    } else {
+      // We reached our target server, but it returned an error
+
+    }
+  };
+
+  var callbacks = {
+    'onload': onLoad
+  };
+
+  makeAjaxRequest(url, callbacks);
+
+};
+
 var handleSearchResults = function(results) {
   renderMovies(results);
 };
 
 var handleSearchInput = function(searchTerm) {
+  SEARCH_RESULTS = [];
   makeSearchRequest(searchTerm);
+};
+
+var handleMovieResults = function(data) {
+  renderMovieDetails(data);
+};
+
+var renderMovieDetails = function(data) {
+  var templateScript = getElementById('movie-details-template').innerHTML;
+  var template = Handlebars.compile(templateScript);
+  getElementById(DETAILS_CONTAINER).innerHTML = template(data);
+
 };
 
 var renderMovies = function(data) {
   var templateScript = getElementById('movie-template').innerHTML;
   var template = Handlebars.compile(templateScript);
-  getElementBySelector('.results-container').innerHTML = template(data);
+  getElementById(RESULTS_CONTAINER).innerHTML = template(data);
+
+
+  var movieContainers = document.querySelectorAll('.movie-container');
+  for (var i = 0, l = movieContainers.length; i < l; i++) {
+    movieContainers[i].addEventListener('click', function(e) {
+      var id = this.getAttribute('data-movieId');
+      makeMovieRequest(id);
+    });
+  }
+
 }
 
-getElementById('search-term').addEventListener('input', function(e) {
-  var searchTerm = encodeURIComponent(getElementById('search-term').value);
+getElementById('search-button').addEventListener('click', function(e) {
+  e.preventDefault();
+  var searchTerm = encodeURIComponent(getElementById(SEARCH_TERM_CONTAINER).value);
   handleSearchInput(searchTerm);
 });
+
+
+
 
